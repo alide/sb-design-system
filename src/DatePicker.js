dd79d2e31f4ae4017a80d90b2957daa0184ca53a
Linh Tran <linh.tran@leapdev.io>
2021-01-15 09:51:36 +1100

    First commit

---
+++ src/DatePicker.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import './datePicker.css';
+
+import { DateRangePicker } from 'react-date-range';
+
+export function DatePicker({ block, ...props }) {
+  const handleSelect = (ranges) => {
+    console.log(ranges);
+  };
+
+  const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  };
+
+  const [ranges, setRanges] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ]);
+
+  return (
+    <DateRangePicker
+      ranges={ranges}
+      onChange={(item) => setRanges([item.selection])}
+      moveRangeOnFirstSelection={false}
+      dateDisplayFormat="P"
+      months={2}
+      direction="horizontal"
+      rangeColors={['#dceef9']}
+      className="leap-4d-date-picker"
+    />
+  );
+}
+
+DatePicker.propTypes = {
+  block: PropTypes.bool,
+};
+
+DatePicker.defaultProps = {
+  block: false,
+};
---
+++ src/DatePicker.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import styled, { css } from 'styled-components';
+
+import { DatePicker } from './DatePicker';
+
+export default {
+  title: 'Design System/DatePicker',
+  component: DatePicker,
+};
+
+export const DateRange = (args) => (
+  <>
+    <DatePicker {...args} />
+  </>
+);
+
+DateRange.args = {
+  block: true,
+};
